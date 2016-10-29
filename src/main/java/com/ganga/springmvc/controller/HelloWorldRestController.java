9a7ff50df41c9111a32721b15bb0c747a34a5452
Gangadhar Pathipaka <gpathipaka@gmail.com>
2016-10-28 21:04:46 -0400

    hello

--- src/main/java/com/ganga/springmvc/controller/HelloWorldRestController.java
+++ src/main/java/com/ganga/springmvc/controller/HelloWorldRestController.java
@@ -1,5 +1,8 @@
 package com.ganga.springmvc.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,6 +11,7 @@ import com.ganga.springmvc.domain.Player;
 
 @RestController
 public class HelloWorldRestController {
+	Map map=new HashMap<String,Integer>();
 
 	@RequestMapping("/hello/{player}")
 	public Player message(@PathVariable String player) {
